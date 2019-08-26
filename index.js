ab779fd71e9e5b5203d5e2e2b7f4513bf3838dc3
Dmitry Yv <df.creative@gmail.com>
2019-08-26 17:43:45 -0400

    Flatten min into a single file

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 import $ from './$.js'
-import './src/min.js'
-// import use from "./use"
-// import fx from "./fx"
-// import html from "./html"
-// import attr from "./attr"
-// import state from "./state"
-// import on from "./on"
-// import mount from "./mount"
-// import init from "./init"
-
-// Object.assign($.fn, {
-//   use, fx, html, attr, state, on, mount, init
-// })
 
 export default $
