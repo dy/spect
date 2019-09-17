072b444c2c8a7f43594301bfc8801a91be6a25ca
Dmitry Yv <df.creative@gmail.com>
2019-09-17 15:27:05 -0400

    Fix html/fx package

--- packages/fx/readme.md
+++ packages/fx/readme.md
@@ -1,147 +1,23 @@
-# @spect/core ![unstable](https://img.shields.io/badge/stability-unstable-yellow) [![Build Status](https://travis-ci.org/spectjs/spect.svg?branch=master)](https://travis-ci.org/spectjs/spect)
+# @spect/fx ![unstable](https://img.shields.io/badge/stability-unstable-yellow) [![Build Status](https://travis-ci.org/spectjs/spect.svg?branch=master)](https://travis-ci.org/spectjs/spect)
 
 Reactive [aspects](https://en.wikipedia.org/wiki/Aspect-oriented_programming) for javascript objects.
 
 
-[![npm i spect](https://nodei.co/npm/spect.png?mini=true)](https://npmjs.org/package/spect/)
+[![npm i @spct/fx](https://nodei.co/npm/@spct/fx.png?mini=true)](https://npmjs.org/package/@spct/fx/)
 
 ```js
-import spect, { state } from 'spect'
+import spect from '@spect/core'
+import fx from '@spect/fx'
 
 spect.fn(state)
 
 let foo = {}
 foo = spect(foo)
 
-// timer aspect
-foo.use(foo => {
-  console.log(foo.state('count'))
-
-  // rerender
-  setTimeout(() => foo.state( state => state.count++ ), 1000)
-})
-```
-
-
-## API
-
-[**`spect.fn`**](#spectfn-fns----register-effects)&nbsp;&nbsp; [**`spect`**](#spect-target---create-aspectable)&nbsp;&nbsp; [**`.use`**](#use-fns---assign-aspects)&nbsp;&nbsp; [**`.update`**](#update-fns-deps----update-aspects)&nbsp;&nbsp; [**`.dispose`**](#dispose-fns----remove-aspect)&nbsp;&nbsp; [**`.fx`**](#fx-------bool--deps---generic-side-effect)&nbsp;&nbsp; [**`.state`**](#state-name--val-deps---getset-state)&nbsp;&nbsp; [**`.prop`**](#prop-name--val-deps---getset-properties)&nbsp;&nbsp;
-
-### `spect.fn( ...fns )` - register effects
-
-Register effect(s) available for targets.
-
-```js
-import spect, { state, prop, fx } from 'spect'
-import { html, css, attr } from 'spect-dom'
-
-spect.fn(state, prop, fx, html, css, attr)
-
-let target = spect(document.querySelector('#my-element'))
-
-// use effects
-target.attr('foo', 'bar')
-target.html`...markup`
-target.css`...styles`
-```
-
-### `spect( target? )` − create aspectable
-
-Turn target into aspectable. The wrapper provides transparent access to target props, extended with registered effects via Proxy. `use` and `update` effects are provided by default, other effects must be registered via `spect.fn(...fxs)`.
-
-```js
-import spect from 'spect'
-
-let target = spect({ foo: 'bar' })
-
-// properties are transparent
-target.foo // 'bar'
-
-// targets are thenable
-await target.use(() => { /* ..aspect */ })
-
-// re-update all aspects
-target.update()
-```
-
-### `.use( fns? )` − assign aspects
-
-Assign aspect(s) to target. Each aspect `fn` is invoked as microtask. By reading/writing effects, aspect subscribes/publishes changes, causing update.
-
-```js
-import spect, { prop, state } from 'spect'
-
-spect.fn(prop, state)
-
-let foo = spect({})
-let bar = spect({})
-
-foo.use(foo => {
-  // subscribe to updates
-  let x = foo.state('x')
-  let y = bar.prop('y')
-
-  // update after 1s
-  setTimeout(() => foo.state( state => state.x++ ), 1000)
-})
-
-// update foo
-bar.prop('y', 1)
-```
-
-### `.update( fns?, deps? )` - update aspect(s)
-
-(re-)Run assigned aspects. If `fn` isn't provided, rerenders all aspects. `deps` control the conditions when the aspect must be reupdate, they take same signature as `useEffect` hook.
-
-```js
-import spect from 'spect'
-
-let foo = spect({})
-
-foo.use(a, b)
-
-// update only a
-await foo.update(a)
-
-// update all
-await foo.update()
-```
-
-### `.dispose( fns? )` - remove aspect
-
-Remove assigned aspects. If `fn` isn't provided, removes all aspects.
-
-```js
-import spect from 'spect'
-
-let foo = spect({})
-
-foo.use(a, b)
-
-// remove a
-await foo.dispose(a)
-
-// remove all
-await foo.dispose()
-```
-
-
-### `.fx( () => (() => {})? , bool | deps? )` − generic side-effect
-
-Run effect function as microtask, conditioned by `deps`. Very much like [`useEffect`](https://reactjs.org/docs/hooks-effect.html) with less limitations, eg. it can be nested into condition. Boolean `deps` can be used to organize toggle / FSM that triggers when value changes to non-false, which is useful for binary states like `visible/hidden`, `disabled/enabled` etc.
-
-```js
-import spect, { fx } from 'spect'
-
-spect.fn(fx)
-
-
-let foo = spect()
-
 // called each time
 foo.fx(() => {});
 
-// called on init only
+// called on init only (within an aspect)
 foo.fx(() => {}, []);
 
 // destructor is called any time deps change
@@ -149,121 +25,13 @@ foo.fx(() => () => {}, [...deps]);
 
 // called when value changes to non-false
 foo.fx(() => { show(); return () => hide(); }, visible);
-```
 
 
-### `.state( name | val, deps? )` − get/set state
-
-Read or write state associated with target. Reading subscribes current aspect to changes of that state. Writing rerenders all subscribed aspects. Optional `deps` param can define trigger condition, see `.fx`.
-
-```js
-import spect, { state } from 'spect'
-
-spect.fn(state)
-
-
-// write state
-$foo.state('foo', 1)
-$foo.state({ foo: 1 })
-
-// mutate/reduce
-$foo.state(s => s.foo = 1)
-$foo.state(s => ({...s, foo: 1}))
-
-// init
-$foo.state({foo: 1}, [])
-
-// read
-$foo.state('foo')
-$foo.state()
-```
-
-
-### `.prop( name | val, deps? )` − get/set properties
-
-Read or write target properties. Same as `.state`, but provides access to element properties.
-
-```js
-import spect, { prop } from 'spect'
-
-spect.fn(prop)
-
-
-// write prop
-$foo.prop('foo', 1)
-$foo.prop({foo: 1})
-
-// mutate/reduce
-$foo.prop(p => p.foo = 1)
-$foo.prop(p => ({...p, foo: 1}))
-
-// init
-$foo.prop({foo: 1}, [])
-
-// read
-$foo.prop('foo')
-$foo.prop()
-```
-
-### Standalone effects
-
-Effects can be used on their own, without `spect`:
-
-```js
-import { fx, state, prop } from 'spect'
-
-let foo = {x: 1}
-
-state.call(foo, 'y', 2)
-prop.call(foo, 'x', 3)
-
-fx.call(foo, () => {
-  state.call(foo, 'y') // 2
-  state.call(foo, 'x') // 3
+// standalone
+fx.call(target, () => {
+  // ...effect code
 })
 ```
-
-
-<!--
-#### Internals
-
-Internal methods are available for effects as
-
-```js
-import spect, { symbols } from 'spect'
-
-spect.fn(function myEffect (arg, deps) {
-  // `this` is `spect` instance
-  // `this[symbols.target]` - initial target object
-
-  // `this._deps(deps, destructor)` - is dependencies gate
-  if (!this._deps(deps, () => { /* destructor */})) return this
-
-  // `this._pub(path)` - publishes update of some name / path string
-  // `this._sub(path, aspect?)` - subscribes current aspect to paths
-  // `this[symbols.subscription]` - subscriptions dict
-  // `this._update(aspect)` - updates aspect as microtask
-  // `this[symbols.promise]` - internal queue
-  // `this[symbols.aspects]` - internal map of assigned aspects
-
-  return this
-})
-```
--->
-
-
-## Changelog
-
-Version | Changes
----|---
-6.0.0 | DOM-less core. Pluggable effects.
-5.0.0 | Wrapper as aspect argument, along with props for react-compatible API. Effect queues.
-4.0.0 | Functional effects API design.
-3.0.0 | References + proxy-based API design.
-2.0.0 | Global effects API design.
-1.0.0 | HTM compiler remake with support for anonymous attributes, html-comments and unclosed tags.
-0.0.1 | [jsxify](https://github.com/scrapjs/jsxify) R&D.
-0.0.0 | Mod framework (Modifiers for DOM).
 
 ##
 
---
+++ packages/html/test/index.js
@@ -0,0 +1,770 @@
+import t from 'tst'
+import spect, { current } from '..'
+
+
+t.skip('counter', t => {
+  let $n = spect({
+    x: 1
+  })
+
+  $n.use($n => {
+    if (!$n.count) $n.count = 0
+
+    console.log($n.count)
+    setTimeout(() => {
+      $n.count++
+      $n.update()
+    }, 1000)
+  })
+})
+
+t('readme: spect(target)', async t => {
+  let log = []
+  let target = spect({ foo: 'bar' })
+
+  // properties are transparent
+  t.is(target.foo, 'bar')
+
+  // registered effects shade properies
+  target.use(() => {
+    log.push(0)
+  })
+
+  // effects are awaitable
+  let x = await spect({})//.fx(() => { log.push(1) }, [])
+
+  t.is(log, [0])
+})
+
+t.skip('readme: use', async t => {
+  let log = []
+
+  let foo = spect({})
+  let bar = spect({y: 0})
+
+  foo.state('x', 0)
+
+  await foo.use(foo => {
+    // subscribe to updates
+    let x = foo.state('x')
+    let y = bar.prop('y')
+
+    log.push(x, y)
+
+    setTimeout(() => foo.state(state => state.x++), 1)
+  })
+
+  t.is(log, [0, 0])
+
+  await new Promise((ok) => {
+    setTimeout(() => {
+      ok()
+    }, 2)
+  })
+
+  t.is(log, [0, 0, 1, 0])
+
+  // update foo
+  bar.prop('y', 1)
+
+  t.is(log, [0, 0, 1, 0])
+  await ''
+
+  t.is(log, [0, 0, 1, 0, 1, 1])
+})
+
+t('readme: update', async t => {
+  let log = []
+
+  let foo = spect({})
+
+  function a () { log.push('a') }
+  foo.use([a, () => log.push('b')])
+
+  t.is(log, [])
+
+  await foo
+
+  t.is(log, ['a', 'b'])
+
+  // update only a
+  await foo.update(a)
+
+  t.is(log, ['a', 'b', 'a'])
+
+  // update all
+  await foo.update()
+
+  t.is(log, ['a', 'b', 'a', 'a', 'b'])
+})
+
+t.skip('readme: fx', async t => {
+  let log = []
+
+  let foo = spect()
+
+  await foo.use(() => {
+    // called each time
+    foo.fx(() => log.push(1));
+
+    // called on init only
+    foo.fx(() => log.push(2), []);
+
+    // destructor is called any time deps change
+    foo.fx(() => (log.push(3), () => log.push(4)), [log[3]]);
+
+    // called when value changes to non-false
+    foo.fx(() => { log.push(5); return () => log.push(6); }, log.length === 0);
+  })
+
+  t.is(log, [1, 2, 3, 5])
+
+  await foo.update()
+
+  t.is(log, [1, 2, 3, 5, 1, 4, 3, 6])
+})
+
+t.skip('readme: state', async t => {
+  let foo = spect()
+
+  await foo.use(() => {
+    // write state
+    foo.state('a', 1)
+    foo.state({ b: 2 })
+
+    // mutate/reduce
+    foo.state(s => s.c = 3)
+    foo.state(s => ({...s, d: 4}))
+
+    // init
+    foo.state({ e: 5 }, [])
+  })
+
+  t.is(foo.state('a'), 1)
+  t.is(foo.state('b'), 2)
+  t.is(foo.state('c'), 3)
+  t.is(foo.state('d'), 4)
+  t.is(foo.state('e'), 5)
+  t.is(foo.state(), {a: 1, b: 2, c: 3, d: 4, e: 5})
+})
+
+t.skip('readme: prop', async t => {
+  let foo = spect()
+
+  await foo.use(() => {
+    // write prop
+    foo.prop('a', 1)
+    foo.prop({ b: 2 })
+
+    // mutate/reduce
+    foo.prop(s => s.c = 3)
+    foo.prop(s => ({ ...s, d: 4 }))
+
+    // init
+    foo.prop({ e: 5 }, [])
+  })
+
+  t.is(foo.prop('a'), 1)
+  t.is(foo.prop('b'), 2)
+  t.is(foo.prop('c'), 3)
+  t.is(foo.prop('d'), 4)
+  t.is(foo.prop('e'), 5)
+  t.is(foo.prop(), { a: 1, b: 2, c: 3, d: 4, e: 5 })
+})
+
+t('readme: destroy', async t => {
+  let log = []
+
+  let foo = spect({})
+
+  foo.x = 0
+
+  await foo.use(foo => {
+    log.push(foo.x)
+  })
+
+  t.is(log, [0])
+
+  await foo.update()
+
+  t.is(log, [0, 0])
+
+  await foo.dispose().update()
+
+  t.is(log, [0, 0])
+})
+
+t.skip('readme: standalone effects', async t => {
+  const { state, fx, prop } = fxs
+
+  let foo = { x: 1 }
+
+  state.call(foo, 'y', 2)
+  prop.call(foo, 'x', 3)
+
+  fx.call(foo, () => {
+    t.is(state.call(foo, 'y'), 2)
+    t.is(prop.call(foo, 'x'), 3)
+  })
+})
+
+t.skip('core: empty / primitive selectors', t => {
+  let $x = spect()
+  let $x1 = spect()
+  t.is($x !== $x1, true)
+
+  $x.state('x', 1)
+  t.is($x.state('x'), 1)
+
+  let $y = spect('xyz')
+  let $y1 = spect('xyz')
+  t.is($y !== $y1, true)
+
+  $y.state('y', 1)
+  t.is($y.state('y'), 1)
+
+  let $z = spect(null)
+  let $z1 = spect(null)
+  t.is($z !== $z1, true)
+
+  $z.state('y', 1)
+  t.is($z.state('y'), 1)
+})
+
+t.todo('registering effects', t => {
+
+})
+
+t('use: aspects must be called in order', async t => {
+  let log = []
+  let a = {}
+  await spect(a).use([() => log.push(1), () => log.push(2), () => log.push(3)])
+  t.deepEqual(log, [1, 2, 3])
+})
+
+t('use: duplicates are ignored', async t => {
+  let log = []
+
+  await spect({}).use([fn, fn, fn])
+
+  function fn() {
+    log.push(1)
+  }
+
+  t.is(log, [1])
+
+  await spect({}).use([fn, fn, fn])
+
+  t.is(log, [1, 1])
+})
+
+t('use: aspects must not be called multiple times, unless target state changes', async t => {
+  let log = []
+
+  let $a = spect({})
+  await $a.use(fn)
+  t.is(log, ['x'])
+  await $a.use(fn)
+  t.is(log, ['x'])
+  await $a.use([fn, fn])
+  t.is(log, ['x'])
+  await $a.update()
+  t.is(log, ['x', 'x'])
+
+  function fn(el) { log.push('x') }
+})
+
+t.skip('use: same aspect different targets', t => {
+  let log = []
+  function fx([el]) {
+    log.push(el.tagName)
+    // return () => log.push('destroy ' + el.tagName)
+  }
+
+  let $el = spect({tagName: 'A'}).use(fx)
+
+  t.is($el.target.tagName, log[0])
+  t.is(log, ['A'])
+
+  $el.target.innerHTML = '<span></span>'
+  $($el.target.firstChild).use(fx)
+
+  t.deepEqual(log, ['A', 'SPAN'])
+})
+
+t('use: Same target different aspects', async t => {
+  let log = []
+
+  let a = {}
+
+  let afx, bfx
+  await spect(a).use([afx = () => (log.push('a'), () => log.push('de a'))])
+  t.deepEqual(log, ['a'])
+  await spect(a).use([bfx = () => (log.push('b'), () => log.push('de b'))])
+  t.deepEqual(log, ['a', 'b'])
+})
+
+t('use: same aspect same target', async t => {
+  let log = []
+  let a = {}
+
+  let fx = () => (log.push('a'), () => log.push('z'))
+  await spect(a).use(fx)
+  t.deepEqual(log, ['a'])
+  await spect(a).use(fx)
+  t.deepEqual(log, ['a'])
+  await spect(a).use(fx)
+  t.deepEqual(log, ['a'])
+})
+
+t('use: subaspects init themselves independent of parent aspects', async t => {
+  let log = []
+
+  let a = {b:{c:{}}}
+  let b = a.b
+  let c = b.c
+
+  await spect(a).use(el => {
+    log.push('a')
+    spect(b).use(el => {
+      log.push('b')
+      spect(c).use(el => {
+        log.push('c')
+        // return () => log.push('-c')
+      })
+      // return () => log.push('-b')
+    })
+    // return () => log.push('-a')
+  })
+
+  t.deepEqual(log, ['a', 'b', 'c'])
+
+  // $.destroy(a)
+
+  // t.deepEqual(log, ['a', 'b', 'c', '-c', '-b', '-a'])
+})
+
+t.todo('use: generators aspects')
+
+t('use: async aspects', t => {
+  let a = spect({})
+
+  a.use(async function a() {
+    t.is(a, current.fn)
+    await Promise.resolve().then()
+    t.is(a, current.fn)
+  })
+})
+
+t.skip('use: promise', async t => {
+  let to = new Promise(ok => setTimeout(ok, 100))
+
+  to.then()
+
+  spect({}).use(to)
+})
+
+
+t.skip('fx: global effect is triggered after current callstack', async t => {
+  let log = []
+  spect({}).fx(() => log.push('a'))
+
+  t.is(log, [])
+
+  await 0
+
+  t.is(log, ['a'])
+})
+
+t.skip('fx: updates destructor', async t => {
+  let log = []
+  let $a = spect({})
+
+  let id = 0
+  let fn = () => {
+    // called each time
+    $a.fx(() => {
+      log.push('init 1')
+      return () => log.push('destroy 1')
+    })
+
+    // called once
+    $a.fx(() => {
+      log.push('init 2')
+      return () => log.push('destroy 2')
+    }, [])
+
+    // called any time deps change
+    $a.fx(() => {
+      log.push('init 3')
+      return () => log.push('destroy 3')
+    }, [id])
+  }
+
+  $a.use(fn)
+  await Promise.resolve().then()
+  t.is(log, ['init 1', 'init 2', 'init 3'])
+
+  log = []
+  $a.update()
+  await Promise.resolve().then()
+  t.is(log, ['destroy 1', 'init 1'])
+
+  log = [], id = 1
+  $a.update()
+  await Promise.resolve().then()
+  t.is(log, ['destroy 1', 'init 1', 'destroy 3', 'init 3'])
+})
+
+t.skip('fx: toggle deps', async t => {
+  let log = []
+  let $a = spect()
+
+  $a.use(() => {
+    $a.fx(() => {
+      log.push('on')
+      return () => log.push('off')
+    }, !!$a.prop('on'))
+  })
+
+  t.is(log, [])
+  await Promise.resolve().then()
+
+  $a.prop('on', false)
+  await Promise.resolve().then()
+  t.is(log, [])
+
+  $a.prop('on', true)
+  await Promise.resolve().then()
+  t.is(log, ['on'])
+
+  $a.prop('on', true)
+  await Promise.resolve().then()
+  t.is(log, ['on'])
+
+  $a.prop('on', false)
+  await Promise.resolve().then()
+  t.is(log, ['on', 'off'])
+
+  $a.prop('on', false)
+  await Promise.resolve().then()
+  t.is(log, ['on', 'off'])
+
+  $a.prop('on', true)
+  await Promise.resolve().then()
+  t.is(log, ['on', 'off', 'on'])
+
+  $a.prop('on', true)
+  await Promise.resolve().then()
+  t.is(log, ['on', 'off', 'on'])
+
+  $a.prop('on', false)
+  await Promise.resolve().then()
+  t.is(log, ['on', 'off', 'on', 'off'])
+})
+
+t.skip('fx: async fx chain', async t => {
+  let log = []
+  let a = spect().fx(() => log.push(1)).fx(() => log.push(2))
+  t.is(log, [])
+  await a.fx(() => log.push(3))
+  t.is(log, [1, 2, 3])
+})
+
+t.skip('fx: async fx', async t => {
+  let log = []
+
+  let el = spect().use(async () => {
+    await el.fx(async () => {
+      await null
+      log.push('foo')
+      return () => {
+        log.push('unfoo')
+      }
+    })
+  })
+  await el
+
+  t.is(log, ['foo'])
+
+  await el.update()
+
+  t.is(log, ['foo', 'unfoo', 'foo'])
+})
+
+t.todo('fx: generator fx')
+
+t.todo('fx: promise')
+
+t.todo('fx: varying number of effects')
+
+t.todo('fx: remove all effects on aspect removal')
+
+t.skip('state: direct get/set', t => {
+  spect().use(el => {
+    el.state('c', 1)
+
+    t.is(el.state('c'), 1)
+  })
+})
+
+t.skip('state: object set', t => {
+  spect().use(el => {
+    el.state({ c: 1, d: 2 })
+
+    t.is(el.state('c'), 1)
+  })
+})
+
+t.skip('state: functional get/set', t => {
+  let a = spect()
+
+  a.state(s => s.count = 0)
+
+  t.is(a.state(), { count: 0 })
+
+  a.state(s => s.count++)
+  t.is(a.state`count`, 1)
+})
+
+t.skip('state: init gate', async t => {
+  let log = [], x = 1
+  let $a = spect()
+
+  await $a.use(fn)
+
+  t.is($a.state`x`, 1)
+
+  x++
+  $a.update()
+  t.is($a.state`x`, 1)
+
+  function fn($a) {
+    $a.state({ x }, [])
+  }
+})
+
+t.skip('state: reducer', t => {
+  let $a = spect({})
+
+  $a.state({ x: 1 })
+
+  let log = []
+  $a.state(s => {
+    log.push(s.x)
+  })
+
+  t.is(log, [1])
+})
+
+t.todo('state: get/set path', t => {
+  let $a = spect()
+
+  t.is($a.state('x.y.z'), undefined)
+
+  $a.state('x.y.z', 1)
+  t.is($a.state(), { x: { y: { z: 1 } } })
+  t.is($a.state('x.y.z'), 1)
+})
+
+t.skip('state: reading state registers any-change listener', async t => {
+  let log = []
+  let $a = spect()
+
+  await $a.use($el => {
+    let s = $el.state()
+
+    log.push(s.x)
+  })
+  t.is(log, [undefined])
+
+  await $a.state({ x: 1 })
+
+  t.is(log, [undefined, 1])
+})
+
+t.skip('state: recursion on both reading/setting state', async t => {
+  let log = []
+  await spect().use($el => {
+    log.push($el.state('x'))
+    // alert($el.state('x'))
+    $el.state({ x: 1 })
+  })
+
+  t.is(log, [undefined, 1])
+})
+
+t.skip('state: same-effect different paths dont trigger update', async t => {
+  let log = []
+  await spect().use($el => {
+    log.push($el.state('x'))
+    $el.state('x')
+    $el.state({y: 1})
+  })
+  t.is(log, [undefined])
+})
+
+t('core: awaiting doesn\'t cause recursion', async t => {
+  let log = []
+
+  let $a = spect()
+  await $a
+
+  let $a2 = $a.use(() => {log.push(2)})
+  log.push(1)
+  t.is($a2, $a)
+  t.is(log, [1])
+  await ''
+  t.is(log, [1,2])
+})
+
+t.skip('core: await returns promise', async t => {
+  let x = await spect()
+  console.log(x.then)
+  t.is(!!x.then, true)
+})
+
+t.skip('state: reading state from async stack doesnt register listener', async t => {
+  let log = []
+  let $a = spect().use($el => {
+    log.push(1)
+    setTimeout(() => {
+      $el.state('x')
+    })
+  })
+  $a.state('x', 1)
+
+  await $a
+
+  // console.log($a, $a.fx(()=>{}))
+  t.is(log, [1])
+
+  await new Promise((ok, nok) => setTimeout(() => {
+    t.is(log, [1])
+    ok()
+  }, 10))
+})
+
+t.skip('state: deps cases', async t => {
+  let el = spect({})
+
+  let x, y, z, w
+
+  x=y=z=w=1
+  await el.use(() => {
+    el.state('x', x, [])
+
+    el.state('y', y)
+
+    el.state('z', z, [el.state('x')])
+
+    el.state('w', w, !!(el.state('y') - 1))
+  })
+
+  t.is(el.state('x'), 1)
+  t.is(el.state('y'), 1)
+  t.is(el.state('z'), 1)
+  t.is(el.state('w'), undefined)
+
+  x=y=z=w=2
+  await el.update()
+
+  t.is(el.state('x'), 1)
+  t.is(el.state('y'), 2)
+  t.is(el.state('z'), 1)
+  t.is(el.state('w'), 2)
+})
+
+t.skip('state: destructuring', async t => {
+  let log = []
+  await spect().use(({state}) => {
+    // init/get state
+    let { foo = 1, bar } = state()
+
+    log.push('get foo,bar', foo, bar)
+
+    // set registered listener updates element
+    state({ foo: 'a', bar: 'b' })
+
+    log.push('set foo,bar')
+  })
+
+  t.deepEqual(log, ['get foo,bar', 1, undefined, 'set foo,bar', 'get foo,bar', 'a', 'b', 'set foo,bar'])
+})
+
+t.skip('state: direct get/set', async t => {
+  let log = []
+  await spect().use(({state}) => {
+    let s = state()
+
+    log.push('get foo,bar', s.foo, s.bar)
+
+    Object.assign(s, { foo: 'a', bar: 'b' })
+
+    log.push('set foo,bar')
+  })
+
+  t.deepEqual(log, ['get foo,bar', undefined, undefined, 'set foo,bar'])
+})
+
+t.skip('state: shared between aspects', async t => {
+  let log = []
+  let el = spect()
+
+  el.use(({state}) => {
+    log.push('a')
+    state({ foo: 'bar' })
+  })
+
+  await el.use(({state}) => {
+    let { foo } = state()
+    log.push('b', foo)
+  })
+
+  t.deepEqual(log, ['a', 'b', 'bar'])
+})
+
+t.skip('prop: direct get/set', t => {
+  spect().use(el => {
+    el.prop('c', 1)
+    t.is(el.c, 1)
+  })
+})
+
+t.skip('prop: object set', t => {
+  spect().use(el => {
+    el.prop({ c: 1, d: 2 })
+
+    t.is(el.prop`c`, 1)
+  })
+})
+
+t.skip('prop: functional get/set', t => {
+  let $a = spect()
+
+  $a.prop(s => s.count = 0)
+
+  t.is($a.prop(), $a)
+
+  $a.prop(s => {
+    s.count++
+  })
+  t.is($a.count, 1)
+})
+
+t.skip('prop: counter', t => {
+  let stop = 0
+  spect().use($a => {
+    $a.prop({ count: 0 }, [])
+
+    console.log($a.prop('count'))
+    $a.fx(s => {
+      if (stop < 6) {
+        setTimeout(() => {
+          $a.prop(s => s.count++)
+          stop++
+        }, 1000)
+      }
+    }, [$a.prop`count`])
+  })
+})
