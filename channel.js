8b67a78449455c92501c0de5de1737655700f2d4
dy <df.creative@gmail.com>
2020-02-14 18:45:40 -0500

    Make canceled flag

--- channel.js
+++ channel.js
@@ -5,6 +5,7 @@ export default function channel(callback, teardown) {
   let resolve, promise = new Cancelable(r => resolve = r)
 
   const ch = (...args) => {
+    if (ch.isCanceled) return
     resolve(...args)
     promise = new Cancelable(r => resolve = r)
     return callback && callback.call && callback(...args)
@@ -20,12 +21,13 @@ export default function channel(callback, teardown) {
     },
     cancel(reason) {
       if (teardown && teardown.call) teardown(reason)
+      ch.isCanceled = true
       return promise.cancel(reason)
     },
     then(...args) {
       return promise.then(...args)
-    }
-
+    },
+    isCanceled: false
     // Observable
     // async subscribe(cb) { for await (let value of ref) cb(value) }
   })
