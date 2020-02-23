30054ae6b54dda66cfde4850ec20fa7517c27c56
dy <df.creative@gmail.com>
2020-02-23 06:00:01 -0500

    Simplify calc

--- calc.js
+++ calc.js
@@ -1,23 +1,25 @@
 import state from './state.js'
 import fx from './fx.js'
-import { getval } from './src/util.js'
 
 export default function calc(fn, deps) {
-  let init = deps.map(getval)
-  const value = state(fn(...init))
+  let value, set
 
-  const set = value.set
-  value.set = () => {
-    throw Error('Setting calculated value')
-  }
-
-  // dfx logic, with initial prev
   fx((...args) => {
-    // some deps may keep reference, so avoid change check
-    // if (args.every((dep, i) => Object.is(dep, prev[i]))) return
-    // prev = args
-    set(fn(...args))
-  }, deps)
+    if (!value) {
+      value = state(fn(...args))
+      set = value.set
+      value.set = () => {
+        throw Error('Setting calculated value')
+      }
+    }
+
+    else {
+      // some deps may keep reference, so avoid change check
+      // if (args.every((dep, i) => Object.is(dep, prev[i]))) return
+      // prev = args
+      set(fn(...args))
+    }
+  }, deps, true)
 
   return value
 }
