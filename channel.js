189fdd241dcf20d179575fb88baec14890bf2a2b
dy <df.creative@gmail.com>
2020-03-08 14:13:15 -0400

    Make observer fn

--- channel.js
+++ channel.js
@@ -1,10 +1,11 @@
-const CANCEL = null
+export const CANCEL = null
 
 export default (...subs) =>
     (val =>
         val === CANCEL ? subs.length = 0 :
-        typeof val === 'function' ?
-        (subs.push(val), () => subs.splice(subs.indexOf(val) >>> 0, 1)) :
+        observer(val) ?
+        (val = val.next || val, subs.push(val), () => subs.splice(subs.indexOf(val) >>> 0, 1)) :
         subs.map(sub => sub(val))
     )
 
+export const observer = (val) => val && (val.call || val.next)
