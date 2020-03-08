c1ec19851dd2df6a29a25bc067eaa2d0067e0102
dy <df.creative@gmail.com>
2020-03-08 14:59:02 -0400

    Use channel in value

--- value.js
+++ value.js
@@ -1,16 +1,16 @@
-const CANCEL = null
+import channel, { observer, CANCEL } from './channel.js'
 
 export default (...subs) => {
   let cur = subs.length ? [subs.shift()] : []
+  let chan = channel()
 
-  value.set = val => (cur[0] = val, subs.map(sub => sub(cur[0])), cur[0])
+  value.set = val => (chan(cur[0] = val), cur[0])
 
   function value (val) {
     if (val === CANCEL) subs.length = 0
 
     let res = !arguments.length ? cur[0] :
-      typeof val === 'function' ?
-      (subs.push(val), cur.length && val(cur[0]), () => subs.splice(subs.indexOf(val) >>> 0, 1)) :
+      observer(val) ? (cur.length && val(cur[0]), chan(val)) :
       value.set(val)
 
     return res
