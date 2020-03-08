a82e68a361cf1398bd6beef64b3879e8e4127652
dy <df.creative@gmail.com>
2020-03-08 14:55:45 -0400

    Make set method

--- value.js
+++ value.js
@@ -2,16 +2,20 @@ const CANCEL = null
 
 export default (...subs) => {
   let cur = subs.length ? [subs.shift()] : []
-  return function (val) {
+
+  value.set = val => (cur[0] = val, subs.map(sub => sub(cur[0])), cur[0])
+
+  function value (val) {
     if (val === CANCEL) subs.length = 0
 
     let res = !arguments.length ? cur[0] :
       typeof val === 'function' ?
       (subs.push(val), cur.length && val(cur[0]), () => subs.splice(subs.indexOf(val) >>> 0, 1)) :
-      (cur[0] = val, subs.map(sub => sub(cur[0])), cur[0])
-
+      value.set(val)
 
     return res
   }
+
+  return value
 }
 
