9c05d2bae62b512fc179bdb1ea53325971d750c6
dy <df.creative@gmail.com>
2020-03-09 16:20:10 -0400

    Make channel support observable proposal

--- channel.js
+++ channel.js
@@ -2,12 +2,26 @@ import _observable from 'symbol-observable'
 
 export default (...subs) => {
     const next = val => subs.map(sub => sub(val))
-    const cancel = () => subs.length = 0
-    const subscribe = val => (val = val.call ? val : val.next, subs.push(val), () => subs.splice(subs.indexOf(val) >>> 0, 1))
+    const cancel = (...unsubs) => (
+        unsubs = subs.map(sub => sub.unsubscribe),
+        subs.length = 0,
+        unsubs.map(unsub => unsub && unsub())
+    )
+    const subscribe = (next, error, complete) => {
+        next = (next.call ? next : next.next).bind()
+        subs.push(next)
+        complete = next.call ? complete : next.complete
+        const unsubscribe = () => (
+            subs.length && subs.splice(subs.indexOf(next) >>> 0, 1),
+            complete && complete(),
+            next.closed = true
+        )
+        next.unsubscribe = unsubscribe.unsubscribe = unsubscribe
+        unsubscribe.closed = false
+        return unsubscribe
+    }
 
-    return Object.assign(val =>
-        observer(val) ? subscribe(val) :
-        next(val)
+    return Object.assign(val => observer(val) ? subscribe(val) : next(val)
     , {
         next,
         subscribe,
