58311230bde8a937f5e7e05fc377632a94939f27
dy <df.creative@gmail.com>
2020-05-10 19:53:30 -0400

    Make tiny VM based h

--- h.js
+++ h.js
@@ -1,8 +1,33 @@
-import { symbol, observable, primitive, attr } from './src/util.js'
+import { symbol, observable, primitive, attr, slice } from './src/util.js'
 
+// DOM
 const TEXT = 3, ELEMENT = 1, ATTRIBUTE = 2, COMMENT = 8, FRAGMENT = 11, SHOW_ELEMENT = 1, SHOW_TEXT = 4, SHOW_COMMENT = 128
-const ZWSP = '\u200B', ZWNJ = '\u200C', ZWJ = '\u200D'
-const FAST = false // FIXME: don't forget to turn it on
+
+// placeholders
+const ZWSP = '\u200B', ZWNJ = '\u200C', ZWJ = '\u200D', FIELD = '\0', FIELD_RE = /\0/g, HTML_FIELD = ZWSP
+
+// FIXME: don't forget to turn it on
+const FAST = true
+
+// evaluate program constants
+// const PROG_TAG = 1,
+//       PROG_COMPONENT = 2,
+//       PROG_QUERY = 3,
+//       PROP_NAME = 4,
+//       PROP_SPREAD = 5,
+//       PROP_VALUE = 6,
+//       PROP_TPL = 7,
+//       PROP_STATIC = 8,
+//       CHILDREN = 9
+const PROG_TAG = ('PROG_TAG'),
+      PROG_COMPONENT = ('PROG_COMPONENT'),
+      PROG_QUERY = ('PROG_QUERY'),
+      PROP_NAME = ('PROP_NAME'),
+      PROP_SPREAD = ('PROP_SPREAD'),
+      PROP_VALUE = ('PROP_VALUE'),
+      PROP_TPL = ('PROP_TPL'),
+      PROP_STATIC = ('PROP_STATIC'),
+      CHILDREN = ('CHILDREN')
 
 // character for node id, ref https://mathiasbynens.be/notes/html5-id-class
 const CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789𘈱'
@@ -10,6 +35,7 @@ const nextChar = c => CHARS.indexOf(c) < CHARS.length - 1 ? CHARS[CHARS.indexOf(
 
 // see also https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
 // const EMPTY = 'area base br col command embed hr img input keygen link meta param source track wbr ! !doctype ? ?xml'.split(' ')
+
 
 const cache = new WeakMap
 
@@ -38,34 +64,46 @@ function buildTemplate (args) {
   const tpl = document.createElement('template'), statics = args[0]
 
   // regex replace is faster, shorter and can identify node by its `<` index in string
-  let str = statics.join('\0')
+  // FIXME: this initial part can be replaced with HTM-like parser (possibly faster?)
+  let str = statics.join(FIELD)
 
-  let ids = Array(str.length), fieldTags = [], prog = [], quotes = [], pathId = '', lvl = 0
+  let ids = Array(str.length), fieldTags = [], prog = [], quotes = [], pathTracker = '', lvl = 0, childFields = {},
+      prevIdx, openIdx, closeIdx, tagStr, id, tplParts = []
 
   // collect nodes with fields - either as attribs or children
+  // getElementById is faster than node path tracking (id is path in a way) https://jsperf.com/queryselector-vs-prop-access
   str = str
     // detecting quotes from inside fields `<a name="a${b}c" ${d}>` is hard, also hiding them makes much easier to detect tags
-    .replace(/".*?"|'.*?'/g, (match, i) => (quotes.push(match), ZWSP))
+    // but we have to keep refs to originals
+    // FIXME: that can be done faster by sequence indexOf's (not sure what I meant)
+    .replace(/".*?"|'.*?'/g, (match, i) => '__' + (quotes.push(match) - 1) + '__')
     // <abc /> → <abc></abc> - makes possible identifying current node level by prev `<`, also no `/` detection in labeling needed
     .replace(/<([\w:-]+)([^>]*)\/>/g, '<$1$2></$1>')
-    // FIXME: use replaceAll when implemented
     // label tags as `<a#a><a#aa/></a><a#b><a#ba></a><a#c/>...` - slicing tail gives parent level id
+    // FIXME: use replaceAll when implemented
     .replace(/</g, (_, idx, str) => {
       // </x>
-      if (str[idx+1] === '/') ids[idx] = pathId.slice(0, --lvl)
+      if (str[idx+1] === '/') ids[idx] = pathTracker.slice(0, --lvl)
       // <!--, <!doctype, <?xml
-      else if (str[idx+1] === '!' || str[idx+1] === '?' || str[idx+1] === '!') ids[idx] = pathId.slice(0, lvl)
+      else if (str[idx+1] === '!' || str[idx+1] === '?' || str[idx+1] === '!') ids[idx] = pathTracker.slice(0, lvl)
       // <x
-      else ids[idx] = (pathId = pathId.slice(0, lvl) + nextChar(pathId[lvl]) + pathId.slice(++lvl)).slice(0, lvl)
-
+      else (
+        childFields[
+          ids[idx] = (pathTracker = pathTracker.slice(0, lvl) + nextChar(pathTracker[lvl]) + pathTracker.slice(++lvl)).slice(0, lvl)
+        ] = 0
+      )
       return '<'
     })
     // FIXME: possible to join with prev method for faster result
     // collect nodes affected by fields
-    .replace(/\0/g, (_,idx,str) => {
-      let openIdx = str.lastIndexOf('<', idx)
-      fieldTags.push(openIdx)
-      return ~str.slice(openIdx, idx).indexOf('>') ? '<!--' + ZWNJ + '-->' : ZWNJ
+    .replace(FIELD_RE, (_,idx,str) => {
+      openIdx = str.lastIndexOf('<', idx)
+      if (prevIdx !== openIdx) fieldTags.push(prevIdx = openIdx)
+      if (~(closeIdx = str.slice(openIdx, idx).indexOf('>'))) {
+        childFields[ids[openIdx]]++
+        return '<!--' + HTML_FIELD + '-->'
+      }
+      return HTML_FIELD
     })
     // // <> → <h:::>
     // .replace(/<(>|\s)/, '<' + FIELD + '$1')
@@ -74,72 +112,96 @@ function buildTemplate (args) {
     // // <a#b.c → <a #b.c
     // .replace(/(<[\w:-]+)([#\.][^<>]*>)/g, '$1 $2')
 
-  // analyze operations for affected nodes - create commands for evaluator
-  // the node access strategy is getElementById https://jsperf.com/queryselector-vs-prop-access
-  // QUERY, id, 1 FIELD
-  // PROPS CREATE
-  // PROP STATIC name, value
-  // PROP VALUE name, 1 FIELD
-  // PROP TPL, name, statics, N FIELDS
-  // PROP SPREAD, 1 FIELD
-  // PROP BOOL, 1 FIELD
-  // NO CHILDREN
-  // SINGLE CHILD, 1 FIELD
-  // CHILDREN, statics, N FIELDS
-  // EVAL
-  for (let i = 0; i < fieldTags.length;) {
-    let idx = fieldTags[i++], id = ids[idx], tagStr = str.slice(idx, str.indexOf('>', idx) + 1)
+  // analyze operations for affected nodes - will be evaluated in build VM
+  for (prevIdx = 0; fieldTags.length;) {
+    openIdx = fieldTags.shift(), id = ids[openIdx], tagStr = str.slice(openIdx + 1, closeIdx = str.indexOf('>', openIdx))
 
-    prog.push(QUERY, id)
+    let parts = tagStr.split(/\s+/), tag = parts.shift()
 
-    // ref https://jsperf.com/replace-vs-split-join-vs-replaceall/95
-    str = str.slice(0, idx) + `<${tag} id=${ids[i]} />` + str.slice(idx + tagStr.length)
+    // PROG_COMPONENT, n attrs, 1 field
+    if (tag === HTML_FIELD) prog.push(PROG_COMPONENT, parts.length)
+    // PROG_QUERY, n attrs, id
+    else if (tag[0] === '#' || tag[0] === '.') prog.push(PROG_QUERY, tag, parts.length)
+    // PROG_TAG, n attrs, name
+    else prog.push(PROG_TAG, id, parts.length)
 
-    // tagStr.replace(/\0/)
-    tagStr.split(' ')
-    .map((part, i) => {
-      let props = current[2] || (current[2] = {})
-      if (part.slice(0, 3) === '...') {
-        Object.assign(props, arguments[++field])
-      }
+    parts.map(part => {
+      // PROP SPREAD, 1 field
+      if (part.slice(0, 3) === '...') prog.push(PROP_SPREAD)
+      // PROP BOOL, 1 field
+      else if (part[0] === HTML_FIELD) prog.push(PROP_NAME)
       else {
-        [name, value] = part.split('=')
-        props[evaluate(name)] = value ? evaluate(value) : true
+        let [name, value] = part.split('=')
+        if (value) {
+          // unquote
+          if (!'__'.indexOf(value)) value = quotes[value.slice(2,-2)]
+          // PROP VALUE name, 1 field
+          if (value === HTML_FIELD) prog.push(PROP_VALUE, name)
+          // PROP TPL, name, statics, N fields
+          else if (value.includes(HTML_FIELD)) prog.push(PROP_TPL, name, value.split(HTML_FIELD))
+          // PROP STATIC name, value
+          else (prog.push(PROP_STATIC, name, value))
+        }
+        else prog.push(PROP_STATIC, name, value || true)
       }
     })
-    prog.push(``)
-  }
 
-  tpl.innerHTML = str
+    // CHILDREN
+    prog.push(CHILDREN, childFields[id] || 0)
+
+    // ref https://jsperf.com/replace-vs-split-join-vs-replaceall/95
+    tplParts.push(str.slice(prevIdx, openIdx), `<${tag} id="--${id}">`)
+    prevIdx = closeIdx + 1
+  }
+  tplParts.push(str.slice(closeIdx + 1))
+
+  // unquote
+  tpl.innerHTML = tplParts.join('').replace(/__\d+__/, m => quotes[m.slice(2,-2)])
 
   const build = (args) => {
-    let frag = tpl.content.cloneNode(true), i = 0, c, f = 1, stack = [], tmp
+    let frag = tpl.content.cloneNode(true), i = 0, c, f = 1, stack = []
 
     // VM inspired by https://twitter.com/jviide/status/1257755526722662405, see ./test/stacker.html
     // it prepares props/children for h function
     for (; i < prog.length;) {
       c = prog[i++]
 
-      if (c--) if (c--) if (c--) if (c--) if (c--) if (c--) if (c--) if (c--) (1)
-      // CHILDREN
-      else stack.push()
-      // CHILD
-      else stack.push(args[f++])
-      // 4 - PROP ${'name'}
-      else stack[1][args[f++]] = true
-      // 3 - PROP ...${props}
-      else Object.assign(stack[1], args[f++])
-      // 2 - PROP name="a${value}b"
-      else stack[1][prog[f++]] = tpl(prog[f++], args, f, prog[f++])
-      // 1 - PROP name=${value}
-      else stack[1][prog[i++]] = args[f++]
-      // N - QUERY
-      else (stack.push(frag.getElementById(idx[i++])), stack[0].id = '')
-      // N - EVAL
-      else h.apply(null, stack.splice(0))
+      // <a
+      if (c === PROG_TAG) (stack.push(frag.getElementById('--' + prog[i++]), prog[i++] ? {} : null), stack[0].removeAttribute('id'))
+      // <#x
+      else if (c === PROG_QUERY) {yyy}
+      // <${el}, <${Comp}
+      else if (c === PROG_COMPONENT) {xxx}
+      // ${'name'}
+      else if (c === PROP_NAME) stack[1][args[f++]] = true
+      // ...${props}
+      else if (c === PROP_SPREAD) Object.assign(stack[1], args[f++])
+      // name="a${value}b"
+      else if (c === PROP_TPL) stack[1][prog[i++]] = tpl(prog[i++], args, f, prog[i++])
+      // name=${value}
+      else if (c === PROP_VALUE) console.log('val', stack[1][prog[i++]] = args[f++], args)
+      // name=value
+      else if (c === PROP_STATIC) stack[1][prog[i++]] = prog[i++]
+      // end
+      else if (c === CHILDREN) {
+        let childNodes = stack[0].childNodes, count = prog[i++]
+        if (count) {
+          // >${child}<, >${a}${b}<
+          if (count === childNodes.length) while (count--) stack.push(args[f++])
+          // >a${child}b${child}c<
+          else {
+            // node has static content prerendered, we initialize only field comments
+            for (count = 0; count < childNodes.length; count++) {
+              stack.push(childNodes[count].nodeType === COMMENT && childNodes[count].data === HTML_FIELD ? args[f++] : childNodes[count])
+            }
+          }
+        }
+        // />
+        h.apply(null, stack.splice(0))
+      }
     }
 
-    return frag
+    return frag.childNodes.length === 1 ? frag.firstChild : frag
   }
 
   cache.set(statics, build)
@@ -149,7 +211,7 @@ function buildTemplate (args) {
 
 
 // compact hyperscript
-export function h(tag, props, ...children) {
+export function h(tag, props) {
   // render redirect
   if (typeof tag === 'string') tag = document.createElement(tag)
   if (typeof tag === 'function') {}
@@ -158,20 +220,24 @@ export function h(tag, props, ...children) {
   for (name in props) {
     value = props[name]
     // primitive is more probable also less expensive than observable check
-    if (primitive(value)) prop(el, name, value)
+    if (primitive(value)) prop(tag, name, value)
     else if (observable(value)) {}
     else prop(el, name, value)
   }
 
-  // merge requires slicing arguments, also action is known in advance here, so we do directly
-  // for (let i = 2, arg; i < arguments.length; i++) {
-  //   arg = arguments[i]
-  //   if (arg.nodeType || primitive(arg)) tag.append(arg)
-  //   else if (observable(arg)) {}
-  //   else tag.append(...arg)
-  // }
-  // FIXME: make faster by passing i=2 and args directly as children
-  merge(tag, tag.childNodes, children)
+  // merge requires slicing arguments (slowish), so when also action is known in advance, so we add directly
+  // FIXME: check if that's slow indeed or we can just merge
+  if (arguments.length > 2) {
+    if (tag.childNodes.length) merge(tag, tag.childNodes, slice(arguments, 2))
+    else {
+      for (let i = 2, arg; i < arguments.length; i++) {
+        arg = arguments[i]
+        if (arg.nodeType || primitive(arg)) tag.append(arg)
+        else if (observable(arg)) {}
+        else tag.append(...arg)
+      }
+    }
+  }
 
   return tag
 }
@@ -547,7 +613,7 @@ const merge = (parent, a, b, end = null) => {
 
 
 const insert = (parent, a, b) => {
-  if (a) {
+  if (a != null) {
     if (primitive(a)) parent.insertBefore(document.createTextNode(a), b)
     else if (a.nodeType) parent.insertBefore(a, b)
     else for (a of a) parent.insertBefore(a, b)
@@ -556,7 +622,7 @@ const insert = (parent, a, b) => {
 
 // WARN: the order is different from replaceNode(new, old)
 const replace = (parent, from, to, end) => {
-  if (to) {
+  if (to != null) {
     if (primitive(to)) {
       if (from.nodeType === TEXT) from.data = to; else from.replaceWith(to)
     }
