039f6a3156deb12706e9b0352ee6edd3917cd9b2
dy <df.creative@gmail.com>
2020-05-14 12:49:21 -0400

    Make more compact

--- test/libs/parse.js
+++ test/libs/parse.js
@@ -9,7 +9,7 @@ const H_TAG = 'h--tag'
 
 export default (statics) => {
   // FIXME: <> → <comp>
-  let mode = TEXT, buf = '', quote = '', tmp, char,
+  let mode = TEXT, buf = '', quote = '', attr, char,
       // transformed statics
       parts = [], part,
       // current element program (id/query, props, children type)
@@ -34,25 +34,21 @@ export default (statics) => {
 
 			else if (char === '>') {
         // <//>, </> → </comp>
-        if (!mode) {
-          if (part.slice(-1) === '/') part = part.slice(0, part.lastIndexOf('<') + 2) + H_TAG
-        }
+        if (!mode && (!buf || buf === '/')) part = part.slice(0, buf ? -buf.length : undefined) + H_TAG
         else if (mode === ELEM) { prog.push(buf), progs.push(prog) }
-        else if (mode === ATTR && tmp && buf) tmp.push(buf)
-        buf = '', tmp = undefined, mode = TEXT
+        else if (mode === ATTR && attr && buf) attr.push(buf)
+        buf = '', attr = undefined, mode = TEXT
       }
       // Ignore everything until the tag ends
-			else if (!mode) {}
+			else if (!mode) buf = char
 
       else if (char === '/') {
         // </x...
-        if (mode === ELEM && !buf) (mode = 0, buf = '/')
+        if (mode === ELEM && !buf) (mode = 0, buf = '')
         // x/> → x />
         else if ((!j || buf) && statics[i][j+1] === '>') { part += ' ' }
         else buf += char
       }
-
-      // else if (char === '=') {}
 			else if (char === ' ' || char === '\t' || char === '\n' || char === '\r') {
         // <a,   <#a ;    ELEM,  field
         // if (mode === ELEM) {
@@ -66,48 +62,38 @@ export default (statics) => {
         //     let id = afterClx.shift()
         //     let clx = [...beforeClx, ...afterClx]
         //     // FIXME
-        //     // if (id) j -= part.length - (part = part.slice(0, tmp + 1) + H_TAG + part.slice(j))
+        //     // if (id) j -= part.length - (part = part.slice(0, attr + 1) + H_TAG + part.slice(j))
         //   }
         //   prog.push(buf), mode = ATTR
         // }
 
         if (mode === ELEM) { prog.push(buf), progs.push(prog), mode = ATTR }
-        else if (mode === ATTR && tmp && buf) { tmp.push(buf) }
-        buf = '', tmp = undefined
+        else if (mode === ATTR && attr && buf) { attr.push(buf) }
+        buf = '', attr = undefined
 			}
 			else buf += char;
 
       // detect comment
-			if (mode === ELEM && buf === '!--') { mode = COMM, part = part.slice(0, -3) }
-      else part += char
+			if (mode === ELEM && buf === '!--') { mode = COMM, part = part.slice(0, -3) } else part += char
 		}
 
     if (++i < statics.length) {
       // >a${1}b${2}c<  →  >a<!--1-->b<!--2-->c<
       if (mode === TEXT) part += '<!---->'
       // <${el} → <h--tag;    ELEM, field
-      else if (mode === ELEM) ((prog.push(i), progs.push(prog)), buf = '', part += H_TAG, mode = ATTR)
-      else if (mode === COMM) {}
+      else if (mode === ELEM) (prog.push(i), progs.push(prog), part += H_TAG, mode = ATTR)
       else if (mode === ATTR) {
         // <xxx ...${{}};    ATTR, null, field
         if (buf === '...') { prog.push(ATTR, null, i), part = part.slice(0, -4) }
         // <xxx ${};    ATTR, field, null
-        else if (!buf && !tmp) { prog.push(ATTR, i, null) }
+        else if (!buf && !attr) { prog.push(ATTR, i, null) }
         else {
           let eq = buf.indexOf('=')
 
-          // <xxx c="a${b}c", <xxx c=a${b}c ;   ATTR, name, [statics, field]
-          if (~eq) {
-            prog.push(ATTR, buf.slice(0, eq), tmp = [])
-            // <xxx c=a${a}
-            if (eq < buf.length - 1) tmp.push(buf.slice(eq + 1))
-            tmp.push(i)
-          }
-          // <xxx x=${};    ATTR, name, field
-          else {
-            if (tmp && buf) tmp.push(buf)
-            tmp.push(i)
-          }
+          // <xxx c="a${b}c", <xxx c=a${b}c ;   ATTR, name, [a, field, b, ...]
+          if (~eq) {prog.push(ATTR, buf.slice(0, eq), attr = []), buf = buf.slice(eq + 1)}
+          if (buf) attr.push(buf)
+          attr.push(i)
 
           part += ZWNJ
         }
