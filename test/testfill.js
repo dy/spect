dc31049df4af8d92b96778c8af8a8cafd57df23a
dy <df.creative@gmail.com>
2020-05-19 19:17:01 -0400

    Reorganize testfill

---
+++ test/testfill.js
@@ -0,0 +1,36 @@
+// test helpers
+Object.defineProperty(DocumentFragment.prototype, 'outerHTML', {
+  get() {
+    let s = '<>'
+    this.childNodes.forEach(n => {
+      s += n.nodeType === 3 ? n.textContent : n.outerHTML != null ? n.outerHTML : ''
+    })
+    s+='</>'
+    return s
+  }
+})
+Object.defineProperty(DocumentFragment.prototype, 'innerHTML', {get() { return this.outerHTML.slice(2,-3) }})
+Object.defineProperty(DocumentFragment.prototype, 'innerHTMLClean', {get() { return this.outerHTMLClean.slice(2,-3) }})
+Object.defineProperty(DocumentFragment.prototype, 'outerHTMLClean', {
+  get() {
+    let s = '<>'
+    this.childNodes.forEach(n => {
+      s += n.nodeType === 3 ? n.textContent : n.outerHTMLClean != null ? n.outerHTMLClean : ''
+    })
+    s+='</>'
+    return s
+  }
+})
+
+Object.defineProperty(Element.prototype, 'innerHTMLClean', {
+  get() {
+    let ihtml = this.innerHTML
+    return ihtml.replace(/<!---->/g, '')
+  }
+})
+Object.defineProperty(Element.prototype, 'outerHTMLClean', {
+  get() {
+    let ohtml = this.outerHTML
+    return ohtml.replace(/<!---->/g, '')
+  }
+})
